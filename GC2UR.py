14cc4a28569d942e8aed13c8634d4f489dad3e12
mka1ugin <mkaluggin@gmail.com>
2020-03-20 16:26:57 +0300

    Added write to file

--- GC2UR.py
+++ GC2UR.py
@@ -1,10 +1,17 @@
 import gcodetools as ur
 
-filename = 'gcode.nc'
+gcode_filename = 'gcode.nc'
+urscript_filename = 'script.urscript'
 
-gcode = open(filename, 'r')
+gcode = open(gcode_filename, 'r')
+urscript = open(urscript_filename, 'w')
 
 for line in gcode:
     script_line = ur.parse_gcode_string(line)
     if script_line:
         print(script_line)
+        urscript.write(script_line + '\n')
+
+urscript.close()
+
+
